0ee195acb9f1965622d686f6033d54fd15f44b81
jiangqk <runner1920@163.com>
2017-11-29 11:56:11 +0800

    基础项目创建

---
+++ common/lemon-common/src/main/java/com/lemon/common/controller/BaseController.java
@@ -0,0 +1,17 @@
+package com.lemon.common.controller;
+
+import org.apache.log4j.Logger;
+
+/**
+ * 
+ * @author jiangqk
+ * @data 2017年8月30日 上午11:43:18
+ */
+public abstract class BaseController {
+	
+	/**
+	 * 公共日志
+	 */
+	protected Logger logger = Logger.getLogger(getClass());
+	
+}
