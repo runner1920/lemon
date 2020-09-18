d1c59c71f73f6eaec249ec5d95e409b03b7f3773
jiangqingkai <runner1920@163.com>
2020-09-18 16:48:24 +0800

    服务注册

--- service/lemon-member/src/main/java/com/lemon/member/controller/MemberController.java
+++ service/lemon-member/src/main/java/com/lemon/member/controller/MemberController.java
@@ -1,38 +1,11 @@
 package com.lemon.member.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.lemon.member.client.OrderClient;
-import com.lemon.member.service.impl.MemberServiceImpl;
 
 /**
  * 
  * @author jiangqk
  * @data 2018年1月5日 下午4:07:02
  */
-@RestController
 public class MemberController {
-	
-	@Autowired
-	private OrderClient orderClient;
-	
-	@Autowired
-	private MemberServiceImpl memberServiceImpl;
-	
-	@GetMapping("/dc")
-	public String execute() {
-		return orderClient.consumer();
-	}
-	
-	@GetMapping("/dc2")
-	public String execute2() {
-		return memberServiceImpl.consumer();
-	}
-	
-	@GetMapping("/member")
-	public String execute3() {
-		return "member success";
-	}
+
 }
