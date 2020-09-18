8fc05d23b74ca0d77361df775463156e756d79f3
jiangqingkai <runner1920@163.com>
2020-09-18 17:17:23 +0800

    配置中心

--- service/lemon-member/src/main/java/com/lemon/member/controller/MemberController.java
+++ service/lemon-member/src/main/java/com/lemon/member/controller/MemberController.java
@@ -1,11 +1,27 @@
 package com.lemon.member.controller;
 
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * 
  * @author jiangqk
  * @data 2018年1月5日 下午4:07:02
  */
+@RestController
+@RefreshScope
 public class MemberController {
 
+    @Value("${txt:null}")
+    private String txt;
+
+    @RequestMapping(value = "/test")
+    public String test() {
+        System.out.println(txt);
+        return txt;
+    }
 }
